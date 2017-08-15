0e2193a66b04e6945ae3350bee0e51a94b4338c6
pschretlen <peter.schretlen@gmail.com>
2017-08-15 02:02:39 +0000

    add nav control and popup

--- src/app/index.js
+++ src/app/index.js
@@ -14,7 +14,11 @@ const map = new mapboxgl.Map({
   scrollZoom: true
 });
 
+const nav = new mapboxgl.NavigationControl()
+
 map.on('load', () => {
+
+    map.addControl(nav, 'top-left');
 
     map.addLayer({
         'id':'boundaries 2017',
@@ -51,7 +55,36 @@ map.on('load', () => {
     const toggleableLayerIds = [ 'boundaries 2007',  'boundaries 2017', ];
     addLayerToggles( toggleableLayerIds )
 
+    // When a click event occurs near a place, open a popup at the location of
+    // the feature, with HTML description from its properties
+    map.on('click', e => {
+        const features = map.queryRenderedFeatures(e.point, { layers: ['boundaries 2017'] });
+
+        // if the features have no info, return nothing
+        if (!features.length) {
+            return;
+        }
+
+        const feature = features[0];
+
+        // Populate the popup and set its coordinates
+        // based on the feature found
+        const popup = new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(`<div id=\'popup\' class=\'popup\' style=\'z-index: 10;\'> <h5> ${feature.properties['address']} </h5> 
+            <span> ${feature.properties['name']} ( ${feature.properties['class']} ) </span></div>`)
+            .addTo(map);
+    });
+
+    // Use the same approach as above to indicate that the symbols are clickable
+    // by changing the cursor style to 'pointer'
+    map.on('mousemove', e => {
+        const features = map.queryRenderedFeatures(e.point, { layers: ['boundaries 2017'] });
+        map.getCanvas().style.cursor = features.length ? 'pointer' : '';
+    });
+
 } )
+
 
 //from: https://www.mapbox.com/mapbox-gl-js/example/toggle-layers/
 function addLayerToggles( ids ){
