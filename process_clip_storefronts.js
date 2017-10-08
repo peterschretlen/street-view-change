da43fb6c2fe01a7ad81999b51738425f2db5748b
pschretlen <peter.schretlen@gmail.com>
2017-10-08 16:48:45 +0000

    create series of offset storefront geom

--- process_clip_storefronts.js
+++ process_clip_storefronts.js
@@ -31,21 +31,41 @@ const roadJSON = fs.readFileSync('./data/mapbox/danforth_road.geojson', "utf8");
 const roadObj = JSON.parse(roadJSON.trim());
 const roadGeom = turf.lineString( roadObj.features[0].geometry.coordinates );
 
-const translatedFeatures = [];
 
-storefronts.features.forEach( f => {
+[...Array(5).keys()].forEach( idx => {
 
-	const point = turf.point( f.geometry.coordinates[0][0] );
-	const nearestPoint = turf.pointOnLine(roadGeom, point, 'kilometers');
+	const translatedFeatures = [];
+	storefronts.features.forEach( f => {
 
-	const bearing = turf.bearing(nearestPoint, point);
+		//Get nearest point to the line for all points. 
+		const nearestPoints = f.geometry.coordinates[0].map( c =>  turf.pointOnLine(roadGeom, turf.point( c ), 'kilometers') )
 
-	//apply translation
-	translatedFeatures.push( turf.transformTranslate(f, 0.006, bearing, 'kilometers') );
+		//Get the nearest and furthest points
+		const distances = {
+			min : Number.MAX_VALUE,
+			max : 0
+		} 
+
+		//Find the shortest & longest distance.
+		nearestPoints.forEach( p => {
+			if( p.properties.dist < distances.min ) distances.min = p.properties.dist;
+			if( p.properties.dist > distances.max ) distances.max = p.properties.dist;
+		})
+
+		//Apply the difference as the translation distance
+		const dist = (idx+1)*(distances.max - distances.min);
+
+		//apply translation
+		const bearing = turf.bearing(nearestPoints[0], turf.point( f.geometry.coordinates[0][0] ));
+		translatedFeatures.push( turf.transformTranslate(f, dist, bearing, 'kilometers') );
+
+	})
+
+	fs.writeFileSync(`./data/processed/clipped_storefronts_translated${idx}.geojson`, JSON.stringify(turf.featureCollection(translatedFeatures)));
 
 })
 
-fs.writeFileSync('./data/processed/clipped_storefronts_translated0.geojson', JSON.stringify(turf.featureCollection(translatedFeatures)));
+
 
 
 
